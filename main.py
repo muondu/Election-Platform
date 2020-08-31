3797a4e55d7b57dada78d4ff804ac4e151c244ce
moundu <munenemuondu@gmail.com>
2020-08-31 10:08:01 +0300

    I have made some changes

---
+++ main.py
@@ -0,0 +1,27 @@
+import time
+import datetime
+now = datetime.datetime.now()
+hour = now.hour
+if hour < 12:
+    print("Good morning")
+elif hour > 12 and hour < 18:
+    print("Good afternoon")
+elif hour > 18 and hour < 19:
+    print("Good evening")
+else:
+    print('Good night.')
+
+what_you__are = input("""Hello. I am the voting program of the future. These are the options available for login. Are you are 
+a Voter
+b Canidate
+
+""")
+def canidate_or_voter():
+    if what_you__are == "a" or what_you__are == "voter" or what_you__are == "A" or what_you__are == "Voter":
+        print("Yaay")
+    elif what_you__are == "b" or what_you__are == "canidate" or what_you__are == "B" or what_you__are == "Candidate":
+        print("Wohhoo")
+    else:
+        print("I did not understand you")
+        canidate_or_voter()
+canidate_or_voter()
