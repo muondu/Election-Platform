1cce94737588fc50485fd7e5ef483c1aa9c629ef
moundu <munenemuondu@gmail.com>
2020-09-02 19:16:44 +0300

    I have done sign up and login

--- login_practise/database.py
+++ login_practise/database.py
@@ -3,4 +3,8 @@ conn = sqlite3.connect("Quiz.db")
 c = conn.cursor()
 
 c.execute('CREATE TABLE IF NOT EXISTS user(username VARCHAR, password VARCHAR)')
-c.execute("INSERT INTO  user (username, password) VALUES('Malli','02')")
+c.execute("INSERT INTO  user (username, password) VALUES('NeroNesh','12')")
+conn.commit()
+
+c.execute('SELECT * FROM user')
+print(c.fetchall())
--- login_practise/main.py
+++ login_practise/main.py
@@ -1,8 +1,48 @@
 import sqlite3
+
 def login():
-    while True:
-        username = input("Enter your username: ")
-        password = input("Enter your password: ")
+    for i in range(4):
+        username = input("Pls enter your username: ")
+        password = input("Pls enter your password: ")
         with sqlite3.connect("Quiz.db") as db:
-            cursor = db.cursor()
-        find_user 
+            cursour = db.cursor()
+        find_user = ('SELECT * FROM user WHERE username = ? AND password = ?')
+        cursour.execute(find_user,[(username),(password)])
+        results = cursour.fetchall()
+
+        if results:
+            for j in results:
+                print("Welcome "+j[0])
+            return "exit"
+            break
+        else:
+            print("Username and password not recognised")
+            again = input("Do you want to try again Yes(y) or No(n)? ")
+            if again.lower == "n":
+                print("GoodBye.")
+def newUser():
+    found = 0
+    while found ==0:
+        username = input("Enter your username: ")
+        with sqlite3.connect("Quiz.db") as db:
+            cursour = db.cursor()
+        findUser = ('SELECT * FROM user WHERE username = ?')
+        cursour.execute(findUser,([username]))
+
+        if cursour.fetchall():
+            print("Username taken, please try again")
+        else:
+            found = 1
+    password = input("Enter your password: ")
+    password1 = input("renter your password: ")
+    while password != password1:
+        print("Your passwords did not match")
+        password = input("Enter your password: ")
+        password1 = input("renter your password: ")
+    insertData = 'INSERT INTO user (username, password) VALUES(?,?)'
+    cursour.execute(insertData,[(username),(password)])
+    db.commit()
+newUser()
+# newUser()
+
+# login()
