457b9410558e341494347df8325639d021e52f02
moundu <munenemuondu@gmail.com>
2020-09-10 12:31:43 +0300

    I have made counting votes

--- staff_members/practise.py
+++ staff_members/practise.py
@@ -1,28 +1,33 @@
 import sqlite3
 conn = sqlite3.connect('practise.db')
 c = conn.cursor()
-c.execute('CREATE TABLE IF NOT EXISTS practise(name TEXT)')
 
 
-username = input("Enter your username: ")
-password = input("Enter your password: ")
-
-c.execute("INSERT INTO practise(name)VALUES('Newton')")  
-
-conn.commit()
-
-# def count_vote():
-for row in c.execute('SELECT * FROM practise'):
-    print(row)
-    votes = list(row)
-    # print(votes)
-    # for core in votes:
-        # print(core)
-    # empty_list = []
-    # appending = empty_list.append(row)
-    # print(empty_list)
-    # counting_votes = votes.count()
-    # print(counting_votes)
+# username = input("Enter your username: ")
+# password = input("Enter your password: ")
+def create():
+    c.execute('CREATE TABLE IF NOT EXISTS practise(name TEXT)')
+    c.execute("INSERT INTO practise VALUES('Core')")  
+    conn.commit()
+create()
 
 
-# count_vote()
+def read():
+    # dub = c.execute('SELECT * FROM practise')
+    c.execute('SELECT * FROM practise')
+    print(c.fetchall())
+    # print(dub)
+read()
+
+def cont():
+    for row in c.execute('SELECT * FROM practise'):
+        list1 = []
+        list1 = list(row)
+        counting = list1.count('Core')
+        print(counting)
+
+
+    # print(list1)
+    # coounting = list1.count("Core")
+    # print(coounting)
+cont()
---
+++ staff_members/practise2.py
@@ -0,0 +1,35 @@
+# # yes = [('Core'),('Unity'),('Unreal engine')]
+engine = [('Core',None),('Unity',None),('Unreal',None)]
+
+results = []
+
+for a in engine:
+    for b in a:
+        results.append(b)
+print(results)
+# jam = [item for a in engine for item in a]
+# print(jam)
+
+# penut_butter = jam.count(1)
+# print(penut_butter)
+
+# occu = jam.count('Core')
+# print(occu)
+
+
+
+
+
+# a = ('Nesh',1)
+# print(type(a))
+# yes = [('a'),('a'),('a')]
+# no = [('a'),('a'),('a')]
+
+# yahoo = []
+
+# for z in yes:
+#     for y in z:
+#         yahoo.append(y)
+# print(yahoo)
+# hada = yahoo.count("aaa")
+# print(hada)
